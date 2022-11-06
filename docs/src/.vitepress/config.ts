7a7cc535bac41036f0b856cad1f4d557603bf03e
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-11-06 15:32:22 +0100

    docs: add plugin api docs page

--- docs/src/.vitepress/config.ts
+++ docs/src/.vitepress/config.ts
@@ -120,6 +120,11 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
         activeMatch: '^/examples/',
       },
       {
+        text: 'Plugins',
+        link: '/plugins/',
+        activeMatch: '^/plugins/',
+      },
+      {
         text: 'TypeDocs',
         link: '/typedocs/',
         activeMatch: '^/typedocs/',
@@ -224,6 +229,13 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
       ],
       '/typedocs/': typedocSidebarEntries(),
       '/changelog/': changelogSidebarEntries(),
+      '/plugins/': [
+        {
+          text: 'Plugins',
+          collapsible: true,
+          items: [],
+        },
+      ],
     },
   },
 })
